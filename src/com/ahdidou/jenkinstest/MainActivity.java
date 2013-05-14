5049e86d53f0e5edf735a1c8f017fe6c98ad5f9e
AHDIDOU <ahdidou.mohamed@gmail.com>
2013-05-14 18:48:13 +0100

    Commentaire

--- src/com/ahdidou/jenkinstest/MainActivity.java
+++ src/com/ahdidou/jenkinstest/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.Menu;
  *
  */
 public class MainActivity extends Activity {
-
+	//Commentaire
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
